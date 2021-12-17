5138d0e2fbf6de53fabfad07e4a319e525ab12b7
Kate <kate.wang2018@gmail.com>
2021-12-17 20:57:46 +0800

    modified some wordings in errors.py

--- cambridge/errors.py
+++ cambridge/errors.py
@@ -7,7 +7,7 @@ class ParsedNoneError(Exception):
     """Used when bs4 returned None whereas there's target content existing within the document"""
 
     def __init__(self):
-        self.message = "Got None wth content in place"
+        self.message = "Parsed out nothing with Cambridge Dictionary content in place"
 
     def __str__(self):
         return self.message
@@ -17,7 +17,7 @@ class NoResultError(Exception):
     """Used when bs4 returned None because Cambridge dict has no result"""
 
     def __init__(self):
-        self.message = "No Result found in Cambridge Dictionary"
+        self.message = "No result found in Cambridge Dictionary"
 
     def __str__(self):
         return self.message

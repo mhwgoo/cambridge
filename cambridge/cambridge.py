338765c20f5a1fdd3ff046863fb8c1fcaa4d7e37
Kate Wang <kate.wang2018@gmail.com>
2022-07-29 16:25:02 +0800

    fixed a typo

--- cambridge/cambridge.py
+++ cambridge/cambridge.py
@@ -13,8 +13,12 @@ The dictionary data comes from https://dictionary.cambridge.org.
 """
 
 CAMBRIDGE_URL = "https://dictionary.cambridge.org"
+
 DICT_BASE_URL = "https://dictionary.cambridge.org/dictionary/english/"  # if no result found in cambridge, response.url is this.
 SPELLCHECK_BASE_URL = "https://dictionary.cambridge.org/spellcheck/english/?q="
+
+DICT_BASE_URL_CHINESE = "https://dictionary.cambridge.org/dictionary/english-chinese-simplified/"
+SPELLCHECK_BASE_URL_CHINESE = "https://dictionary.cambridge.org/spellcheck/english-chinese-simplified/?q="
 
 RESQUEST_URL = ""
 RESPONSE_WORD = ""
@@ -253,7 +257,7 @@ def parse_first_dict(url):
             attempt = call_on_error(e, url, attempt, OP[1])
         else:
             if not first_dict: 
-                attempt = call_on_error(ParsedNoneError(), url, attempt, OP[0])
+                attempt = call_on_error(ParsedNoneError(), url, attempt, OP[1])
 
             RESPONSE_WORD = soup.find("title").text.split("|")[0].strip().lower()
             return first_dict

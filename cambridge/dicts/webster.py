0f001477e236abf6acd58ff5bd449b4e09d2f240
Kate Wang <kate.wang2018@gmail.com>
2023-08-21 14:39:08 +0800

    updated to v3.6.0

--- cambridge/dicts/webster.py
+++ cambridge/dicts/webster.py
@@ -22,7 +22,6 @@ word_entries = []  # A page may have multiple word entries, e.g. "give away", "g
 word_forms = [] # A word may have multiple word forms, e.g. "ran", "running", "run", "flies"
 
 # TODO: green noun 3a, jitter(sense 1), run(with 2 digit item list), up front, blowup, time on, flavor, rolling, entry, give someone up, bling, gravity, entrench, honeybun, wildcard, [on the toes, step on the toes, knock on the door]
-# TODO: update README
 
 # ----------Request Web Resouce----------
 def search_webster(con, cur, input_word, is_fresh=False, no_suggestions=False):
